5a4961eb1a6daf5bea946f54ae19627793ef63e7
Alexander Ho <alexhsz98@gmail.com>
2020-09-13 23:16:09 +0100

    Added Inventory Component, Item Component and related Food Item.

---
+++ Source/UntitledGameProject/Items/FoodItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/Item.h"
+#include "FoodItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLEDGAMEPROJECT_API UFoodItem : public UItem
+{
+	GENERATED_BODY()
+	
+};
